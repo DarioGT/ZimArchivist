88e610f639a958a57fd48c0c7a75f15e73c94bee
François Boulogne <fboulogne at april dot org>
2012-02-05 14:09:48 +0100

    moving files

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+
+setup(
+name         = 'ZimArchivist',
+#version      = info.VERSION,
+#url          = info.URL,
+#author       = author,
+#author_email = info.EMAIL,
+#description  = info.SHORT_DESCRIPTION,
+packages = ['ZimArchivist'],
+scripts     = ['zimarchivist.py'],
+)
