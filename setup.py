2bb0160cc13efd31ca527e0d5f31e8433e41b415
François Boulogne <fboulogne at april dot org>
2012-02-19 20:54:02 +0100

    update info in setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
-
+from ZimArchivist import info
 
 setup(
-name         = 'ZimArchivist',
-#version      = info.VERSION,
-#url          = info.URL,
-#author       = author,
-#author_email = info.EMAIL,
-#description  = info.SHORT_DESCRIPTION,
-packages = ['ZimArchivist'],
-scripts     = ['zimarchivist.py'],
+    name         = 'ZimArchivist',
+    version      = info.VERSION,
+    url          = info.URL,
+    author       = "François Boulogne",
+    author_email = info.EMAIL,
+    description  = info.SHORT_DESCRIPTION,
+    packages = ['ZimArchivist'],
+    scripts     = ['zimarchivist.py'],
 )
