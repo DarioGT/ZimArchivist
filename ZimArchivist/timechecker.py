080ff4961b5f01382d42bf1267493a72022d3e82
François Boulogne <fboulogne@sciunto.org>
2012-08-30 14:36:55 +0200

    transform timechecker to a class

--- ZimArchivist/timechecker.py
+++ ZimArchivist/timechecker.py
@@ -18,38 +18,53 @@ import shelve
 import os
 import time
 
+class TimeChecker():
 
-def set_time(filename):
-    """
-    Put the current time in the database for the filename
-    """
-    database = shelve.open(os.path.expanduser('~/.zimarchivist/time.db'))
-    try:
-        database[filename] = time.time()
-    finally:
-        database.close()
+    def __init__(self, timedb, zimroot):
+        """
+        timedb : database file path 
+        zimroot : root of the zim notebook
+        """
+        self.timedb = os.path.expanduser(timedb)
+        self.zimroot = zimroot
 
+    def set_time(self, filename):
+        """
+        Put the current time in the database for the filename
+    
+        filename : 
+        """
+        database = shelve.open(self.timedb)
+        try:
+            database[filename] = time.time()
+        finally:
+            database.close()
+    
+    
+    def get_file_modif_status(self, filename):
+        """ 
+        Return the file status
 
-def get_file_modif_status(zimroot, filename):
-    """ 
-    Check if the file changed
-    True: The file changed in the mean time
-    True: if we don't know
-    False: The file is the same.
-    """
-    database = shelve.open(os.path.expanduser('~/.zimarchivist/time.db'))
-    try:
-        previous_time = database[filename]
-    except KeyError:
-        #We don't know...
-        return True
-    finally:
-        database.close()
-
-    if os.path.getmtime(os.path.join(zimroot, filename)) < previous_time:
-        return False
-    else:
-        return True
+        Check if the file changed
+        True: The file changed in the mean time
+        True: if we don't know
+        False: The file is the same.
+    
+        filename : 
+        """
+        database = shelve.open(self.timedb)
+        try:
+            previous_time = database[filename]
+        except KeyError:
+            #We don't know...
+            return True
+        finally:
+            database.close()
+    
+        if os.path.getmtime(os.path.join(self.zimroot, filename)) < previous_time:
+            return False
+        else:
+            return True
 
 if __name__ == '__main__':
     pass
