f73692784ba00493da22cea24ab3fbad69ad5238
François Boulogne <fboulogne@sciunto.org>
2012-09-10 20:40:36 +0200

    link license

--- setup.py
+++ setup.py
@@ -8,6 +8,7 @@ setup(
     version      = info.VERSION,
     url          = info.URL,
     author       = "Francois Boulogne",
+    license      = info.LICENSE,
     author_email = info.EMAIL,
     description  = info.SHORT_DESCRIPTION,
     packages = ['ZimArchivist'],
